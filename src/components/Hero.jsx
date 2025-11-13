2a4f22ea2df40e0894816ce1a4691b88eeb55515
Jeremias Petliuk <74077990+jpetliuk@users.noreply.github.com>
2025-11-13 20:46:47 +0100

    fix: add relative positioning to Hero component for improved layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
    return (
       <header
          id="hero"
-         className="w-dvw h-[80vh] sm:h-dvh flex flex-col items-center justify-between bg-[url('/assets/hero-background.webp')] bg-[20%_42%] bg-cover bg-no-repeat px-5"
+         className="w-dvw h-[80vh] sm:h-dvh flex flex-col items-center justify-between bg-[url('/assets/hero-background.webp')] bg-[20%_42%] bg-cover bg-no-repeat px-5 relative"
       >
          <div
             className="
