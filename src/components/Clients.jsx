93a8b10cc1fffa414c2fafed13cd325e1343d5d3
Jeremias Petliuk <74077990+jpetliuk@users.noreply.github.com>
2025-11-14 16:36:43 +0100

    fix: correct class attribute syntax and improve formatting in Clients component

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -17,23 +17,25 @@ const Clients = () => {
          id="Clients"
          className="bg-black flex flex-col gap-10 items-center justify-center w-screen py-20"
       >
-         <h2 className="italic text-[#FEE085] text-3xl sm:text-4xl font-bold">CLIENTS</h2>
+         <h2 className="italic text-[#FEE085] text-3xl sm:text-4xl font-bold">
+            CLIENTS
+         </h2>
 
          <div className="flex items-center sm:mb-4 flex-wrap justify-center sm:mt-12 mt-5">
             <img
                src="./assets/clients-assets/logos/warner-chappell-logo.png"
                alt="Warner Chappell Music Logo"
-               class="h-15 w-40 sm:h-20 sm:w-50 object-contain hover:scale-105 duration-300 cursor-pointer"
+               className="h-15 w-40 sm:h-20 sm:w-50 object-contain hover:scale-105 duration-300 cursor-pointer"
             />
             <img
                src="./assets/clients-assets/logos/klap-logo.png"
                alt="Klap Logo"
-               class="h-15 w-40 sm:h-20 sm:w-50 object-contain hover:scale-105 duration-300 cursor-pointer"
+               className="h-15 w-40 sm:h-20 sm:w-50 object-contain hover:scale-105 duration-300 cursor-pointer"
             />
             <img
                src="./assets/clients-assets/logos/tempos-logo.png"
                alt="Tempos Logo"
-               class="h-15 w-60 sm:h-20 sm:w-65 object-contain hover:scale-105 duration-300 cursor-pointer"
+               className="h-15 w-60 sm:h-20 sm:w-65 object-contain hover:scale-105 duration-300 cursor-pointer"
             />
          </div>
          <div className="flex flex-wrap max-w-7xl items-center justify-center gap-5">
